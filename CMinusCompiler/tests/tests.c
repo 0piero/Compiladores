84f4a0ec612dd260761a71bd26f534fe98a42b77
qedrohenrique <pedro.005henr@gmail.com>
2023-10-10 20:54:10 -0300

    test: updated char type checker test

--- CMinusCompiler/tests/tests.c
+++ CMinusCompiler/tests/tests.c
@@ -6,7 +6,21 @@ void test_char_type_checker(){
         is_special_char('/') &&
         is_special_symbol('*', '/') &&
         !is_special_char('%') &&
-        !is_special_symbol('x', '/')
-    ) printf("[+] test_char_type_checker: PASS!\n");
-    else printf("[-] test_char_type_checker: FAIL!\n");
+        !is_special_symbol('x', '/') &&
+        is_digit('9') &&
+        !is_digit('x') &&
+        is_alpha('x')&&
+        !is_alpha('9')
+    ) {
+        printf("[+] test_char_type_checker: PASS!\n");
+        return;
+    }
+    else {
+        printf("[-] test_char_type_checker: FAIL!\n");
+        exit(1);
+    }
+}
+
+void test_input_reader(){
+
 }
