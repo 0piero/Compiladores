b240f57046bba17346f5a9dbdfee02b381791a5e
Pedro <pedro.005henr@gmail.com>
2023-10-12 06:46:01 -0300

    refactor: moved stuff to generic types

--- CMinusCompiler/utils/generic_types.h
+++ CMinusCompiler/utils/generic_types.h
@@ -1,10 +1,11 @@
-#ifndef TYPES_H
-#define TYPES_H
+#ifndef GENERIC_TYPES_H
+#define GENERIC_TYPES_H
 
 #define TRUE 1
 #define FALSE 0
 
 typedef int boolean;
+typedef int** table;
 
 struct buff {
     int curr_char_pos;
