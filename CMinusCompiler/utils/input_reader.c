75219494f14325506fe82ebcacf42f10432e967d
Pedro <pedro.005henr@gmail.com>
2023-10-16 01:41:37 -0300

    style: identifiers name change

--- CMinusCompiler/utils/input_reader.c
+++ CMinusCompiler/utils/input_reader.c
@@ -1,18 +1,18 @@
 #include "input_reader.h"
 
-void clear_buffer(input_buffer buff){
+void clear_input_buffer(input_buffer buff){
     int i = 0;
     for(i = 0; i < BUFFER_SIZE; i++){
         buff.word_buffer[i] = '\0';
     }
 }
 
-input_buffer create_and_allocate_buffer(){
+input_buffer create_and_allocate_input_buffer(){
     input_buffer buff;
     buff.curr_char_pos = 0;
     buff.curr_line = 1;
     buff.word_buffer = (char*) malloc(BUFFER_SIZE);
-    clear_buffer(buff);
+    clear_input_buffer(buff);
     // printf("[+] allocated buffer\n");
     return buff;
 }
--- CMinusCompiler/utils/input_reader.h
+++ CMinusCompiler/utils/input_reader.h
@@ -9,8 +9,8 @@
 /* Put it here since its related to input size, not buffer structure itself. */
 #define BUFFER_SIZE 32
 
-void clear_buffer(input_buffer buff);
-input_buffer create_and_allocate_buffer();
+void clear_input_buffer(input_buffer buff);
+input_buffer create_and_allocate_input_buffer();
 char get_next_char(input_buffer* buffer, boolean update_char_flag, boolean update_line_flag);
 
 #endif
