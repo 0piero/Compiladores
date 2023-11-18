79cfcbb987066af1a8d48c8387db71aca8bac71c
qedrohenrique <pedro.005henr@gmail.com>
2023-11-18 19:08:12 -0300

    chore: removed comments

--- CMinusCompiler/main.c
+++ CMinusCompiler/main.c
@@ -13,32 +13,3 @@ int main() {
     clean_buffers();
     return 0;
 }
-
-
-// int main(){
-
-//     // run_tests();
-
-//     FILE *fp = fopen("input.txt", "r");
-//     FILE *output = fopen("output.txt", "w");
-
-//     
-
-//     /* Initial state  */
-//     int state = 0; 
-//     int curr_char_idx = -1;
-
-//     do {
-//         char * p_str = fgets(input_buff.word_buffer, BUFFER_SIZE, fp);
-//         char curr_char;
-//         boolean update_char_num = TRUE, update_line_num = TRUE;
-
-
-
-//         input_buff.curr_char_pos = 0;
-//         clear_input_buffer(input_buff);
-//         if(!p_str) break;
-//     } while(1);
-
-  
-// }
