2968afaff56c67be6d4016cc7fefc5942c0fd8c6
0piero <piero.andreeto@unifesp.br>
2023-12-01 13:38:14 -0300

    atualizando types

--- CMinusCompiler/common/types.c
+++ CMinusCompiler/common/types.c
@@ -1,5 +1,5 @@
 #include "types.h"
-
+#include <stdio.h>
 TokenNode* allocate_token_node(){
     TokenNode *t;
     t = malloc(sizeof(TokenNode));
@@ -7,4 +7,17 @@ TokenNode* allocate_token_node(){
     t->lexem = malloc(sizeof(char) * 40);
 
     return t;
+}
+
+void printTokenNode(TokenNode *t){
+    printf(" ============ \n");
+    printf("Token: %s \n", t->token);
+    printf("Lexem: %s \n", t->lexem);
+    printf("Scope: %s \n", t->scope);
+    //printf("Len: %s \n", t->len);
+    printf("NodeType: %d \n", t->nodetype);
+    printf("DataType: %d \n", t->datatype);
+    printf("Line: %d \n", t->line);
+    printf(" ============ \n");
+
 }
--- CMinusCompiler/common/types.h
+++ CMinusCompiler/common/types.h
@@ -3,6 +3,12 @@
 
 #include <stdlib.h>
 
+#define INTEGER_T 0
+#define VOID_T 1
+
+#define FUNCAO 2
+#define VARIAVEL 3
+
 struct TokenNode {
     char * token;   // ID, INT, ...
     char * lexem;   // Lexema puro, ex: int ABC;
@@ -14,5 +20,6 @@ struct TokenNode {
 typedef struct TokenNode TokenNode;
 
 TokenNode* allocate_token_node();
+void printTokenNode(TokenNode*);
 
 #endif
